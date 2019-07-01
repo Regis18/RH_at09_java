045cb859ba2245e794439ac1ab665caaa9fec371
Regis18 <regis.humana@fundacion-jala.org>
2019-07-01 15:36:03 -0400

    Update Person.java

--- quickstart/src/main/java/org/gradle/Person.java
+++ quickstart/src/main/java/org/gradle/Person.java
@@ -2,14 +2,24 @@ package org.gradle;
 
 import org.apache.commons.collections.list.GrowthList;
 
+/**
+* Person.
+* @author Regis Humana.
+*/
 public class Person {
     private final String name;
 
-    public Person(String name) {
+    /**
+     * Constructor.
+     */
+    public Person(final String name) {
         this.name = name;
         new GrowthList();
     }
 
+    /**
+     * Get Name.
+     */
     public String getName() {
         return name;
     }
