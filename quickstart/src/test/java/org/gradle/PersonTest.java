39b7ee700e8e794dc8475541dba402f17456f997
Regis18 <regis.humana@fundacion-jala.org>
2019-07-01 15:50:32 -0400

    Update PersonTest.java

--- quickstart/src/test/java/org/gradle/PersonTest.java
+++ quickstart/src/test/java/org/gradle/PersonTest.java
@@ -1,9 +1,17 @@
 package org.gradle;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+/**
+ * PersonTest.
+ * @author Regis Humana.
+ */
 public class PersonTest {
+
+    /**
+     * Verify if the method can constuct a person with name.
+     */
     @Test
     public void canConstructAPersonWithAName() {
         Person person = new Person("Larry");
